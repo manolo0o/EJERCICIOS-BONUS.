0fd7728b5778ec4177f7b3e10b0eca1e6f8f183d
manolo0o <147361804+manolo0o@users.noreply.github.com>
2024-01-31 15:57:18 -0500

    Update nprimos.py

--- sistema.campues/modulo/nprimos.py
+++ sistema.campues/modulo/nprimos.py
@@ -1,11 +1,52 @@
-#nombre = str(input("Ingrese su nombre : "))
-#print (f"Hola,{nombre}")
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
+# nombre = str(input("Ingrese su nombre : "))
+# print (f"Hola,{nombre}")
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
+# --- Escriba un programa que reciba como entrada el radio de un círculo y entregue como salida su perímetro y su área:
+# import math
+# radio = int(input("INGRESE EL RADIO DEL CIRCULO :"))
+# Perimetro = 2 * math.pi * radio
+# print ("El perimetro del circulo es", Perimetro)
+# area = math.pi * radio**2
+# print ("El area del circulo es", area)
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
+#----Escriba un programa que calcule el promedio de 4 notas ingresadas por el usuario:
+# n1 = int (input(" ingrese la primer nota : "))
+# n2 = int (input(" ingrese la segunda nota : "))
+# n3 = int (input(" ingrese la tercer nota : "))
+# n4 = int (input(" ingrese la cuarta nota : "))
+# promedio = n1 + n2 + n3 + n4 / 4
+# print ("Su promedio de notas es : ", promedio)
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
+#------- Escriba un programa que convierta de centímetros a pulgadas. Una pulgada es igual a 2.54 centímetros.
+# cms = int(input("ingrese los centimentros a convertir : "))  
+# inches = cms * 2.54 
+# print (f"{cms} centimetros son iguales a {inches} pulgadas. " )
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
+#---------Escriba un programa que pida al usuario un entero de tres dígitos, y entregue el número con los dígitos en orden inverso:
+# entero = int(input("Ingrese un número entero de 3 digitos porfavor: "))
+# entero_inverse = int(str(entero)[::-1])
+# print(f"{entero} al reves es :", entero_inverse)
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
 
+# ---------programa que reciba la entrada de dos catetos
+# import math
+# cA = int(input("Ingrese el cateto A :"))
+# cB = int(input("Ingrese el cateto B :"))
+# hC = math.sqrt (cA** 2 + cB**2)
+# print ("La longitud de la hipotenua es", hC)
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
 
-#Escriba un programa que reciba como entrada el radio de un círculo y entregue como salida su perímetro y su área:
-import math
-radio = int(input("INGRESE EL RADIO DEL CIRCULO :"))
-Perimetro = 2 * math.pi * radio
-print ("El perimetro del circulo es", Perimetro)
-area = math.pi * radio**2
-print ("El area del circulo es", area)
+# ------- Realice un programa en el que digite la hora actual "t" y un número entero de horas "h", e indique que hora marcará dentro de "h" horas
+# hrs = int(input("Ingrese el número de horas que van a transcurrir : "))
+# hora_Futura = (hora_Actual + hrs) % 24
+# print (f"Cuando transcurran {hrs} horas seran las {hora_Futura}.")
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+# ------ Escriba un programa que entregue la parte decimal de un número real ingresado por el usuario.
+# import math
+# numero = float(input("Ingrese un número real : "))
+# parte_decimal = math.modf(numero)
+# print("Parte decimal:", parte_decimal)
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
